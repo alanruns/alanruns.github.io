1931e0431e685b41d2f4fd9bd6ac9eb009601e9d
Alan Sun <asun0927@gmail.com>
2018-05-10 22:35:02 -0700

    Finalize Blog Post Fetcher

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -5,8 +5,7 @@
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.onload = function() {
-        debugger;
-        console.log(this.responseXML.title);
+        resolve(this.responseXML.body.firstElementChild);
       }
       xhr.open('GET', `/posts/${dateID}.html`);
       xhr.responseType = "document";
@@ -18,13 +17,18 @@
     let blogDate = moment("2018-05-10");
     const today = moment();
 
+    const mainBody = document.querySelector('body main');
     while(blogDate.isSameOrBefore(today)) {
-      let blogPostText;
+      let blogPost;
       console.log('before await');
       try {
-        blogPostText = await fetchBlogPost(blogDate.format('YYYY-MM-DD')); 
+        blogPost = await fetchBlogPost(blogDate.format('YYYY-MM-DD')); 
       } catch (e) {
-        blogPostText = '';
+        blogPost = '';
+      }
+
+      if (blogPost) {
+        mainBody.appendChild(blogPost);
       }
       console.log('after await');
       console.log(blogPostText);
