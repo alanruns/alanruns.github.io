541426765cc09ed695638f075b2d4a9c04ca4a3f
Alan Sun <asun0927@gmail.com>
2018-05-10 21:45:48 -0700

    Try fetching one blog post

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -1,1 +1,12 @@
 console.log('downloaded!');
+
+function fetchBlogPost(dateID) {
+  return fetch(`posts/${dateID}.html`);
+}
+
+async function getBlogPosts(){
+  let blogPostText;
+
+  blogPostText = fetchBlogPost(10052018);
+  console.log(blogPostText);
+}
